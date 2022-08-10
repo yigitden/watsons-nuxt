725a25489518b6e1fed2d14d56e93710eaaebc13
yigitden <yigitcandeniz@gmail.com>
2022-08-10 18:44:14 +0300

    button added for payment page

--- pages/checkout.vue
+++ pages/checkout.vue
@@ -5,6 +5,10 @@
       <div class="col-span-2 ">
         <ShippingForm/>
         <PaymentDetails/>
+        <div class="w-1/2 mt-4">
+
+        <Button text="PAY NOW"/>
+        </div>
       </div>
       <div class="col-span-1">
         <Summary/>
@@ -19,8 +23,9 @@ import Title from "../components/Checkout/Title.vue";
 import ShippingForm from "../components/Forms/ShippingForm.vue";
 import PaymentDetails from "../components/Forms/PaymentDetails.vue";
 import Summary from "../components/ShoppingCard/Summary.vue";
+import Button from "../components/Button/index.vue";
 export default {
-  components: { Title, ShippingForm, PaymentDetails, Summary },
+  components: { Title, ShippingForm, PaymentDetails, Summary, Button },
 };
 </script>
 
