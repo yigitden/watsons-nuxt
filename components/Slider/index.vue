05f34c7c025c61afc1ae52a11f4e5e7cc64dc733
yigitden <yigitcandeniz@gmail.com>
2022-08-10 18:44:21 +0300

    new slider loaded

--- components/Slider/index.vue
+++ components/Slider/index.vue
@@ -1,74 +1,41 @@
 <template>
-<div id="default-carousel" class="relative" data-carousel="static">
-    <!-- Carousel wrapper -->
-    <div class="relative h-56 overflow-hidden rounded-lg md:h-96 bg-titan">
-         <!-- Item 1 -->
-        <div class=" duration-700 ease-in-out " data-carousel-item>
-             <div class="grid grid-cols-2">
-                <div >
-                    <p class="text-grey font-bold text-sm ">Naturals By Watsons</p>
-                     <p class="text-coal font-bold text-4xl	 ">The new 2021 collection</p>
-                     <p class="text-graphene mt-4 ">Known as "the miracle plant", Aloe Vera helps to nourish, moisturize and keep the hair looking smooth. Let your hair be flexible and fresh.</p>
-                </div>
-                <div>            
-                 <img src="/images/right.png" alt="...">
-                </div>
-            </div>
-        </div>
-        <!-- Item 2 -->
-        <div class=" duration-700 ease-in-out" data-carousel-item>
-            <div class="grid grid-cols-2">
-                <div>
+  <agile>
+    <div class="slide">
+      <div class="grid grid-cols-2 bg-titan px-[140px]">
+        <div class="flex flex-col justify-center ">
+          <p class="font-bold	text-sm text-grey">NATURALS BY WATSONS</p>
+           <p class="text-4xl text-coal font-bold ">The new 2021 collection</p>
+            <p class="text-graphene my-4">Known as "the miracle plant", Aloe Vera helps to nourish, moisturize and keep the hair looking smooth. Let your hair be flexible and fresh.</p>
+           <div>
+             <Button text="SHOP NOW" padding="py-2 px-4"/>
 
-                </div>
-                <div>            
-                 <img src="/images/right.png"  alt="...">
-                </div>
-            </div>
+           </div>
         </div>
-        <!-- Item 3 -->
-        <div class=" duration-700 ease-in-out " data-carousel-item>
-        <div class="grid grid-cols-2">
-                <div></div>
-                <div>            
-                 <img src="/images/right.png"  alt="...">
-                </div>
-            </div>
-             </div>
+        <div>       <img src="/images/right.png"/></div>
+   
+      </div>
+      
     </div>
-    <!-- Slider indicators -->
-    <div class="absolute z-30 flex space-x-3 -translate-x-1/2 bottom-5 left-1/2">
-        <button type="button" class="w-3 h-3 rounded-full" aria-current="true" aria-label="Slide 1" data-carousel-slide-to="0"></button>
-        <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
-        <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
+
+    ...
+
+    <div class="slide">
+      <h3>slide n</h3>
     </div>
-    <!-- Slider controls -->
-    <button type="button" class="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
-        <span class="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30  group-hover:bg-white/50  group-focus:ring-4 group-focus:ring-white group-focus:outline-none">
-            <svg  class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path></svg>
-            <span class="sr-only">Previous</span>
-        </span>
-    </button>
-    <button type="button" class="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
-        <span class="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30  group-hover:bg-white/50  group-focus:ring-4 group-focus:ring-white group-focus:outline-none">
-            <svg  class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path></svg>
-            <span class="sr-only">Next</span>
-        </span>
-    </button>
-</div>
+  </agile>
 </template>
 
 <script>
+  import { VueAgile } from 'vue-agile'
+import Button from '../Button/index.vue'
+
 export default {
-    name:'Slider',
-    mounted() {
-      let sliderScript = document.createElement('script')
-      sliderScript.setAttribute('src', 'https://unpkg.com/flowbite@1.4.0/dist/flowbite.js')
-      document.head.appendChild(sliderScript )
-    },
+  components: {
+    agile: VueAgile,
+    Button
+}
 }
 </script>
-
-<style>
+<style scoped>
 
 </style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -44,12 +44,13 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
+
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
     postcss: {
-      plugins: {
+      plugins: { 
         tailwindcss: {},
         autoprefixer: {},
       },
