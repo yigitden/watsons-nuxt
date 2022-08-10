0b15a2d7fef1e43654a2a716e11e9a9bf6d858c6
yigitden <yigitcandeniz@gmail.com>
2022-08-10 18:43:42 +0300

    new props added in button compenent

--- components/Button/index.vue
+++ components/Button/index.vue
@@ -1,11 +1,11 @@
 <template>
-  <button class="bg-primary p-4 rounded text-white">{{text}}</button>
+  <button class="bg-primary p-4 rounded text-white" :class="{ [padding]:padding }">{{text}}</button>
 </template>
 
 <script>
 export default {
     name:'Button',
-    props:['text']
+    props:['text','padding']
 }
 </script>
 
