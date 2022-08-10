7855b14cd22796e0b96fb81ddb78b8df8ceaf5de
yigitden <yigitcandeniz@gmail.com>
2022-08-10 12:37:30 +0300

    input component changed

--- components/Input/index.vue
+++ components/Input/index.vue
@@ -1,10 +1,19 @@
 <template>
 <div>
+     <label
+          :for="id"
+          class="block mb-2 text-sm font-medium text-gray-900"
+          >{{labelText}}</label
+        >
+        <input
+          :type="type" :id="id" 
+          class="border border-smoke text-gray-900 text-sm rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+          :placeholder="placeholder"
+          :pattern="pattern"
+          required=""
+        />
 
- <input :type="type" :id="id" 
-  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500
-   focus:border-blue-500 block w-full p-2.5" 
-   :placeholder="placeholder" required>
+ 
     
 </div>
  
@@ -14,7 +23,7 @@
 <script>
 export default {
     name:'Input',
-    props:['placeholder','type','id']
+    props:['placeholder','type','id','labelText','pattern']
 
 }
 </script>
