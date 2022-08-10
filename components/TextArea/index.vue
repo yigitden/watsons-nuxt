914b58acd43a4e795119355d5079cd7ff7286d78
yigitden <yigitcandeniz@gmail.com>
2022-08-10 12:43:52 +0300

    Textarea component added

---
+++ components/TextArea/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+     <label
+        :for="id"
+        class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
+        >{{labelText}}</label
+      >
+      <textarea
+        :id="id" 
+        :rows="rows"
+        class="block p-2.5 w-full text-sm text-gray-900  rounded border border-smoke focus:ring-blue-500 focus:border-blue-500"
+        :placeholder="placeholder"
+      ></textarea>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'TextArea',
+    props:['id','labelText','rows','placeholder']
+}
+</script>
+
+<style scoped>
+    textarea{
+        resize:none
+    }
+</style>
