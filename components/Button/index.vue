1d808eb94bdeaf646f35e01ab29ce9572881fe5f
yigitden <yigitcandeniz@gmail.com>
2022-08-10 11:01:57 +0300

    button component added

---
+++ components/Button/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <button class="bg-primary p-4 rounded text-white">{{text}}</button>
+</template>
+
+<script>
+export default {
+    name:'Button',
+    props:['text']
+}
+</script>
+
+<style>
+
+</style>
