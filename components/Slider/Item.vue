c4e7b623d22f696e2078cafedd9139b27e1500c6
yigitden <yigitcandeniz@gmail.com>
2022-08-10 18:53:35 +0300

    Slider item component added and fix slider.vue

---
+++ components/Slider/Item.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="grid grid-cols-2 bg-titan px-[140px]">
+    <div class="flex flex-col justify-center">
+      <p class="font-bold text-sm text-grey">{{firstLine}}</p>
+      <p class="text-4xl text-coal font-bold">{{secondLine}}</p>
+      <p class="text-graphene my-4">
+        {{thirdLine}}
+      </p>
+      <div>
+        <Button :text="buttonText" padding="py-2 px-4" />
+      </div>
+    </div>
+    <div>
+      <img :src="image" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'Item',
+    props:['image','buttonText','firstLine','secondLine','thirdLine']
+};
+</script>
+
+<style></style>
--- components/Slider/index.vue
+++ components/Slider/index.vue
@@ -1,41 +1,45 @@
 <template>
   <agile>
     <div class="slide">
-      <div class="grid grid-cols-2 bg-titan px-[140px]">
-        <div class="flex flex-col justify-center ">
-          <p class="font-bold	text-sm text-grey">NATURALS BY WATSONS</p>
-           <p class="text-4xl text-coal font-bold ">The new 2021 collection</p>
-            <p class="text-graphene my-4">Known as "the miracle plant", Aloe Vera helps to nourish, moisturize and keep the hair looking smooth. Let your hair be flexible and fresh.</p>
-           <div>
-             <Button text="SHOP NOW" padding="py-2 px-4"/>
 
-           </div>
-        </div>
-        <div>       <img src="/images/right.png"/></div>
-   
-      </div>
-      
+
+      <Item
+      image="/images/right.png"
+      buttonText="SHOP NOW"
+      firstLine="NATURALS BY WATSONS"
+      secondLine="The new 2021 collection"
+      thirdLine="Known as 'the miracle plant', Aloe Vera helps to nourish, moisturize and
+        keep the hair looking smooth. Let your hair be flexible and fresh."
+      />
     </div>
 
-    ...
+
 
     <div class="slide">
-      <h3>slide n</h3>
+           <Item
+      image="/images/right.png"
+      buttonText="TRY IT"
+      firstLine="WATSONS LAB TURKIYE"
+      secondLine="SAMPLE SLIDER LAB TURKIYE"
+      thirdLine="Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
+      />
     </div>
+   
   </agile>
 </template>
 
 <script>
-  import { VueAgile } from 'vue-agile'
-import Button from '../Button/index.vue'
+import { VueAgile } from "vue-agile";
+import Button from "../Button/index.vue";
+import Item from "./Item.vue";
 
 export default {
   components: {
     agile: VueAgile,
-    Button
-}
-}
+    Button,
+    Item
+},
+};
 </script>
-<style scoped>
-
-</style>
+<style scoped></style>
