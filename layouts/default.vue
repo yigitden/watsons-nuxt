a66ce3c813ce78e687f8c12f212698add274994b
yigitden <yigitcandeniz@gmail.com>
2022-08-10 11:16:39 +0300

    remove about page and added header to layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,14 @@
 <template>
   <div>
+    <Header/>
     <nuxt/>
   </div>
 </template>
 
 <script>
+import Header from '../components/Header/index.vue';
 export default {
-
+    components: { Header }
 }
 </script>
 
