7cad5bfc04ef0c81f41c139c73c677200ac00f34
yigitden <yigitcandeniz@gmail.com>
2022-08-12 13:14:06 +0300

    new components for user on mobile

---
+++ components/Header/MobileUserMenu.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="bg-smoke30 divide-y ">
+    <MobileUserMenuItem text="My profile" :icon="['fas', 'user']" />
+    <MobileUserMenuItem text="Order history" :icon="['fas', 'inbox']" />
+    <MobileUserMenuItem text="Help center" :icon="['fas', 'circle-question']" />
+    <MobileUserMenuItem text="Log out" :icon="['fas', 'right-from-bracket']" />
+  </div>
+</template>
+
+<script>
+import MobileUserMenuItem from "./MobileUserMenuItem.vue";
+export default {
+  components: { MobileUserMenuItem },
+};
+</script>
+
+<style></style>
---
+++ components/Header/MobileUserMenuItem.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="text-graphene flex items-center justify-end p-4 pt-4 pb-2 cursor-pointer hover:bg-smoke50 duration-500">
+    <p class="text-sm font-medium mr-4 ">{{ text }}</p>
+    <font-awesome-icon :icon="icon" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["text", "icon"],
+};
+</script>
+
+<style></style>
