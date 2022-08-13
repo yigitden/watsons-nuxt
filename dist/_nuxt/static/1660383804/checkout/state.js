25ecd80c5490b91e961631174a71dea8f36bf28e
yigitden <yigitcandeniz@gmail.com>
2022-08-13 23:40:45 +0300

    change route for dist

--- dist/_nuxt/static/1660383804/checkout/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fwatsons-nuxt\u002F_nuxt\u002Fstatic\u002F1660383804",layout:"default",error:a,serverRendered:true,routePath:"\u002Fcheckout",config:{_app:{basePath:"\u002Fwatsons-nuxt\u002F",assetsPath:"\u002Fwatsons-nuxt\u002F_nuxt\u002F",cdnURL:a}}}}(null));
